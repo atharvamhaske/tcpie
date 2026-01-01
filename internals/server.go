905d7713b8721e40dadcdc840181e2e421b4a25f
atharvamhaske <atharvamhaske76@gmail.com>
2026-01-01 16:54:49 +0530

    server file

---
+++ internals/server.go
@@ -0,0 +1,86 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/atharvamhaske/tcpie/internals/metrics"
+	"github.com/atharvamhaske/tcpie/internals/rate-limiter"
+)
+
+// for accepting tcp connections
+type Server struct {
+	WorkerPool
+	Port       int
+	URL        string
+	Opts       ServerOpts
+	Metrics    metrics.ServerMetrics
+	Listener   net.Listener
+	reqLimiter ratelimiter.TokenBucket
+}
+
+type ServerOpts struct {
+	Rate       int64
+	Tokens     int64
+	MaxThreads int
+	QueueSize  int
+}
+
+func (s *Server) createListener() {
+	log.Println("creating a listener")
+	addr := s.URL + ":" + fmt.Sprintf("%d", s.Port)
+	socket, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("server connection failed with %v", err)
+	}
+	s.Listener = socket
+}
+
+func (s *Server) createThreadPool() {
+	log.Println("creating thread pool")
+	s.WorkerPool = WorkerPool{MaxWorkers: s.Opts.MaxThreads, QueueSize: s.Opts.QueueSize}
+	s.NewWorkerPool()
+}
+
+func (s *Server) createRateLimiter(rate, token int64) {
+	log.Println("creating a rate limiter")
+	s.reqLimiter = ratelimiter.RateLimiter(rate, token)
+}
+
+func (s *Server) handleRequest() {
+	log.Println("start handling requests")
+
+	conn := 0
+
+	for {
+		client, err := s.Listener.Accept() //accept clients
+		conn++
+		log.Printf("client connected %d", conn)
+		if err != nil {
+			log.Fatalf("%v", err)
+		}
+		log.Println("processing the request")
+
+		if s.reqLimiter != (ratelimiter.TokenBucket{}) && s.reqLimiter.IsReqAllowed() {
+			s.Metrics.Requests.WithLabelValues("processed").Inc()
+			s.SubmitJob(Job{
+				Id:   conn,
+				Conn: client,
+			})
+		}
+	}
+}
+
+// Close closes the socket listener and worker pool
+func (s *Server) Close() {
+	s.Listener.Close()
+	s.WorkerPool.Close()
+}
+
+func (s *Server) FireUpTheServer() {
+	s.createListener()
+	s.createThreadPool()
+	s.createRateLimiter(s.Opts.Rate, s.Opts.Tokens)
+	s.handleRequest()
+}
