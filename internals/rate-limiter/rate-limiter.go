3bfcbf9beb9bb0372b197e45a98d0101ed8d86c6
atharvamhaske <atharvamhaske76@gmail.com>
2026-01-01 16:00:28 +0530

    fmt

--- internals/rate-limiter/rate-limiter.go
+++ internals/rate-limiter/rate-limiter.go
@@ -17,14 +17,14 @@ type TokenBucket struct {
 
 func RateLimiter(rate, tokens int64) TokenBucket {
 	return TokenBucket{
-		MaxTokens: tokens,
-		Rate: rate,
+		MaxTokens:  tokens,
+		Rate:       rate,
 		LastRefill: time.Now(),
-		Mutex: &sync.Mutex{},
+		Mutex:      &sync.Mutex{},
 	}
 }
 
-//this method puts tokens in bucket
+// this method puts tokens in bucket
 func (tb *TokenBucket) refillBucket() {
 	log.Println("Refilling the bucket")
 	now := time.Now()
@@ -32,21 +32,21 @@ func (tb *TokenBucket) refillBucket() {
 	last := time.Since(tb.LastRefill)
 
 	tokenstoAdd := (last.Milliseconds() * tb.Rate) / 1000
-	log.Printf("adding %f tokens to bucket at %v", float64(tb.Tokens + tokenstoAdd), now)
+	log.Printf("adding %f tokens to bucket at %v", float64(tb.Tokens+tokenstoAdd), now)
 
-	tb.Tokens = int64(math.Min(float64(tb.Tokens + tokenstoAdd), float64(tb.MaxTokens)))
+	tb.Tokens = int64(math.Min(float64(tb.Tokens+tokenstoAdd), float64(tb.MaxTokens)))
 
 	tb.LastRefill = now
 }
 
-//method to check is request allowed or should be dropped
+// method to check is request allowed or should be dropped
 func (tb *TokenBucket) isReqAllowed() bool {
 	tb.Mutex.Lock()
 	defer tb.Mutex.Unlock()
 
 	tb.refillBucket()
 	if tb.Tokens > 0 {
-		tb.Tokens --
+		tb.Tokens--
 		return true
 	}
 	return false
